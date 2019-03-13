f42adc53a088c606a26e710fb5cecaee095aeca7
Zokero <piotrekkukiela@gmail.com>
2019-03-13 16:25:07 +0100

    Add MultiUsers Server

--- src/com/company/Server/StorageServer.java
+++ src/com/company/Server/StorageServer.java
@@ -1,9 +1,6 @@
 package com.company.Server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -28,20 +25,14 @@ public class StorageServer {
                 Runnable worker = new RequestHandler(socket);
                 executor.execute(worker);
             }
-
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.println("Client connected on port " + portNumber + " Servicing request");
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Received message " + inputLine + " from " + socket.toString());
-                out.println(inputLine);
-            }
-
-
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port"
                     + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }finally {
+            if(executor != null){
+                executor.shutdown();
+            }
         }
     }
 }
